63205cecea59a652338759efb8f900e15224acaa
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-27 09:16:39 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(:name => params[:user][:name])
-      if @user != nil && @user.authenticate(params[:user][:password])
+      if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
       end
   end
