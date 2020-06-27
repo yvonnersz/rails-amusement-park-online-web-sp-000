eeedb1f07f20baf068c1490f4866d47e0c74b2c7
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-27 09:19:23 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :happiness, :nausea, :height, :tickets)
+    params.require(:user).permit(:name, :password, :happiness, :nausea, :height, :tickets, :admin)
   end
 end
