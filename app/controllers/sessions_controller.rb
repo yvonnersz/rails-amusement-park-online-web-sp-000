1ea340f49736aa7e2e2f3c2c7603ea1af4f572dd
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-27 09:00:35 +0000

    Automatically backed up by Learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+  def new
+    @users = User.all
+  end
+
+  def create
+    @user = User.find_by(:name => params[:user][:name])
+      if @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root 'users#new'
+
+  get '/users/new' => 'users#new'
+  post '/' => 'users#create'
+
+  get '/users/:id' => 'users#show'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
